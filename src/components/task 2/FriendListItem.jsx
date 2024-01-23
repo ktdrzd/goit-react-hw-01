eff749778d60ab405b5cf25d79b9a9566133b818
ktdrzd <137915871+ktdrzd@users.noreply.github.com>
2024-01-23 18:07:39 +0200

    Update FriendListItem.jsx

--- src/components/task 2/FriendListItem.jsx
+++ src/components/task 2/FriendListItem.jsx
@@ -1,14 +1,6 @@
 import style from './FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
-  const statusClasses = ['status'];
-  if (isOnline) {
-    statusClasses.push('online');
-  } else {
-    statusClasses.push('offline');
-  }
-
-  //   console.log(statusClasses.join(" "));
 
   return (
     <div className={style.item}>
